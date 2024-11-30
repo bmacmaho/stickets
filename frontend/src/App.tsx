f316a1ec3fd665d0838b5670c292fc5f9d1833d7
BarraGit <macmahonbarra+git@gmail.com>
2024-11-30 17:04:33 +0100

    add event overview

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import Login from "./scenes/Login/Login";
 import Home from "./scenes/Home/Home";
+import EventOverview from "./scenes/EventOverview/EventOverview";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isBuyTicketsOpen, setBuyTicketsIsOpen] = useState(false)
 
   return (
     <>
       { !isLoggedIn && <Login setIsLoggedIn={setIsLoggedIn} /> }
-      { isLoggedIn && <Home/>}
+      { isLoggedIn && !isBuyTicketsOpen && <Home setBuyTicketsIsOpen={setBuyTicketsIsOpen}/>}
+      { isLoggedIn && isBuyTicketsOpen && <EventOverview setBuyTicketsIsOpen={setBuyTicketsIsOpen}/>}
     </>
   );
 }
